d115c7c252cdfa4bb723b02ba553d8595da5381c
abolfazlmor <abolfazlmor@systemgroup.net>
2025-10-31 23:37:23 +0330

    addeding initial auth service setup

--- auth/internal/respository/postgres/postgres.go
+++ auth/internal/respository/postgres/postgres.go
@@ -1,4 +1,35 @@
 package postgres
 
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
 type PostgreSQL struct {
+	client *gorm.DB
+}
+
+func NewPostgreSQL() *PostgreSQL {
+	dsn := os.Getenv("POSTGRES_DATABASE")
+	if dsn == "" {
+		log.Fatal("POSTGRES_DATABASE environment variable not set")
+	}
+
+	db, err := gorm.Open(postgres.Open(dsn))
+	if err != nil {
+		log.Fatalf("Failed to connect to database: %s", err.Error())
+	}
+
+	return &PostgreSQL{client: db}
+}
+
+func (p *PostgreSQL) Close() error {
+	sqlDB, err := p.client.DB()
+	if err != nil {
+		return err
+	}
+
+	return sqlDB.Close()
 }
